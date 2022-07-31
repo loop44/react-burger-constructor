6c1390b1cad76d306ed013f1763f2e5070d87dca
LOOP <loop404@icloud.com>
2022-07-31 22:51:18 +0400

    Intro marqup fix

--- src/pages/Intro/index.jsx
+++ src/pages/Intro/index.jsx
@@ -1,10 +1,14 @@
+import Header from "../../components/Header";
 import styles from "./Intro.module.scss";
 
 const Intro = () => {
   return (
-    <div className={styles.main}>
-      <h1 className={styles.mainTitle}>Make Your Burger</h1>
-    </div>
+    <>
+      <Header />
+      <div className={styles.main}>
+        <h1 className={styles.mainTitle}>Make Your Burger</h1>
+      </div>
+    </>
   );
 };
 
